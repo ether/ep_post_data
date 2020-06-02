7af19ea736b101e681c2a2dd437329280563eb0a
John McLear <john@mclear.co.uk>
2020-06-02 16:06:35 +0000

    make etherpad great again...

--- index.js
+++ index.js
@@ -14,27 +14,40 @@ exports.registerRoute = function (hook_name, args, callback) {
       // Append data.
       content += data;
     });
-
-    req.on('end', function () {
-      API.getText(padId, function(err, d) {
-        if (err) {  // pad doesn't exist, create one ...
-          API.createPad(padId, content, function(err, d){
-            if(err){
-              res.send("Error: " + err);
-            }else{
-              res.send("Pad Created: "+fullUrl+"/p/"+padId + " ");
-            }
-          });
-        } else {  // pad exists, update it...
-          API.setText(padId, content, function(err, d){
-            if(err){
-              res.send("Error: " + err);
-            }else{
-              res.send("Pad Updated: "+fullUrl+"/p/"+padId + " ");
-            }
-          });
+    req.on('end', async function () {
+      var padExists = true;
+      try{
+        padExists = await API.getText(padId, 0);
+        if(padExists && padExists.text){
+          padExists = true;
         }
-      });
+      }catch(e){
+        padExists = false;
+      }
+
+      // Pad does not exist so creating a new pad.
+      if(!padExists){
+        try{
+          console.debug("ep_post_data: Creating new pad", padId)
+          await API.createPad(padId, content);
+          res.send("Success creating new pad");
+        }catch(e){
+          console.error("ep_post_data: Error creating pad", padId, e)
+          res.send("Error creating pad");
+        }
+      }
+
+      // Pad already exists so updating an existing pad.
+      if(padExists){
+        try{
+          console.debug("ep_post_data: Setting text!", padId, content)
+          await API.setText(padId, content)
+          res.send("Success updating pad");
+        }catch(e){
+          console.error("ep_post_data: Error updating pad", padId, e)
+          res.send("Error updating pad");
+        }
+      }
     });
   });
 }
